d1f8f03fbead37cbd4334229e30d69ac9e8ed417
Matt Dorchak <matthewdorchak235@gmail.com>
2022-07-27 17:50:36 -0400

    Create the grid and add styling for the boxes

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
 const body = document.querySelector('body')
 const div = document.createElement('div')
 
+div.setAttribute("id", "container")
 body.appendChild(div)
+
+const container = document.querySelector('#container')
+const sideSize = 16
+for (let j = 0; j < sideSize; j++) {
+    let row = document.createElement('div')
+    row.setAttribute("id", "rows")
+    for (let i = 0; i < sideSize; i++) {
+        let d = document.createElement('div')
+        d.setAttribute("class", "boxes")
+        row.appendChild(d)
+    }
+    container.appendChild(row)
+}
