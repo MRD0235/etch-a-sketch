bb92fb91796cd8ff45b16b3617dd8509ab02dcef
Matt Dorchak <matthewdorchak235@gmail.com>
2022-07-28 00:07:16 -0400

    Create buttons for reset and grid size

--- javascript.js
+++ javascript.js
@@ -1,21 +1,47 @@
 const body = document.querySelector('body')
 const div = document.createElement('div')
-
 div.setAttribute("id", "container")
 body.appendChild(div)
 
-const container = document.querySelector('#container')
-const sideSize = 16
-for (let j = 0; j < sideSize; j++) {
-    let row = document.createElement('div')
-    row.setAttribute("id", "rows")
-    for (let i = 0; i < sideSize; i++) {
-        let d = document.createElement('div')
-        d.setAttribute("class", "boxes")
-        d.addEventListener("mouseover", () => {
-            d.setAttribute("id", "colored")
-        })
-        row.appendChild(d)
+const resetButton = document.querySelector('#reset')
+resetButton.addEventListener('click', clearGrid)
+
+const gridButton = document.querySelector('#changeSize')
+gridButton.addEventListener('click', changeSize)
+
+function gridSetup(sideSize) {
+    for (let j = 0; j < sideSize; j++) {
+        let row = document.createElement('div')
+        row.setAttribute("id", "rows")
+        for (let i = 0; i < sideSize; i++) {
+            let d = document.createElement('div')
+            d.setAttribute("class", "boxes")
+            d.addEventListener("mouseover", () => {
+                d.setAttribute("id", "colored")
+            })
+            row.appendChild(d)
+        }
+        container.appendChild(row)
     }
-    container.appendChild(row)
+}
+
+function changeSize() {
+    sideSize = prompt("Set a size for the grid:", 16)
+    clearGrid()
+    removeDiv()
+    gridSetup(sideSize)
+}
+
+function removeDiv() {
+    const parent = document.querySelector('#container')
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+function clearGrid() {
+    let coloredBoxes = document.querySelectorAll('#colored')
+    coloredBoxes.forEach(coloredBox => {
+        coloredBox.removeAttribute("id", "colored")
+    })
 }
